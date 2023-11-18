a198f1a3d0865bf11e22c952f2d653166eeb1dd0
Oscar Laird <olaird25@gmail.com>
2023-11-17 23:42:12 -0500

    dummy node

---
+++ svelte-app/src/DummyNode.svelte
@@ -0,0 +1,24 @@
+<script>
+    // Props received by the component
+    export let fields = {};
+  
+    // Reactive statement to get keys and values from the fields object
+    $: entries = Object.entries(fields);
+  </script>
+  
+<style>
+.rectangle {
+    border: 1px solid black;
+    padding: 10px;
+    margin: 10px;
+    max-width: 200px;
+}
+</style>
+
+<div class="rectangle">
+{#each entries as [key, value]}
+    <div>
+    <strong>{key}:</strong> {value}
+    </div>
+{/each}
+</div>
